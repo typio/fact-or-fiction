df76dd2f6fad7da69751d72ccbd5faef24ea810d
Thomas Huber <me@tohuber.com>
2022-11-11 02:40:48 -0700

    did a lil more structure :fire:

--- build_data.py
+++ build_data.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 import requests
 import concurrent.futures
 
+data = pd.DataFrame(columns=['Claim', 'True'])
+
+
 def request_claim(url, i):
     url = url.replace("PAGE_INDEX", str(i))
     try:
@@ -18,14 +21,27 @@ def request_claim(url, i):
         # snopes
         for tag in soup.select('div.article_info_wrap'):
             print(tag.find(attrs={'class': 'article_title'}).get_text())
-            
 
         text = text.replace("\n", " ")
         text = " ".join(text.split())
 
+        # TODO: add to data
+
     except:
         print(f"Couldn't get {url}")
 
+
+def write_file(str, label, i):
+    if not os.path.exists(f'train'):
+        os.makedirs(f'train')
+
+    filepath = os.path.join(f'train', f"{label}")
+
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    with open(os.path.join(filepath, f"{i}.txt"), "w", encoding="utf-8") as f:
+        f.write(str)
 
 def build_data():
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as e:
@@ -35,18 +51,9 @@ def build_data():
             e.submit(
                 request_claim, "https://www.politifact.com/factchecks/list/?page=PAGE_INDEX&ruling=true", i)
 
-
-
-        if not os.path.exists(f'train'):
-            os.makedirs(f'train')
-
-        filepath = os.path.join(f'train', f"{label}")
-
-        if not os.path.exists(filepath):
-            os.makedirs(filepath)
-
-        with open(os.path.join(filepath, f"{i}.txt"), "w", encoding="utf-8") as f:
-            f.write(text)
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as e:
+        for row in data:
+            e.submit(
+                write_file, row['Label'], i)
 
 build_data()
